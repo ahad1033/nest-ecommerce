aa10a281f14882258d33138eecbd22c2ee8187e6
ahad1033 <a.ahad2232@gmail.com>
2024-03-02 19:05:28 +0600

    added our performance section

---
+++ src/Components/OurPerformance.jsx
@@ -0,0 +1,28 @@
+const OurPerformance = () => {
+  return (
+    <div className="flex flex-col lg:flex-row px-2 lg:px-10 mt-6 mb-11">
+      <div className="w-full lg:w-1/2">
+        <img src="/ourPerformance.png" alt="" />
+      </div>
+      <div className="flex flex-col justify-center w-full lg:w-1/2 px-2 lg:px-10 mt-11 lg:mt-0">
+        <p className="font-secondary text-secondary mb-2">Our Performance</p>
+        <h1 className="w-2/3 font-primary font-bold text-3xl lg:text-4xl leading-10 text-primary mb-5">
+          Your Partner for e-commerce grocery solution
+        </h1>
+        <p className="font-secondary text-secondary mb-2">
+          Ed ut perspiciatis unde omnis iste natus error sit voluptatem
+          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
+          ab illo inventore veritatis et quasi architecto
+        </p>
+        <br />
+        <p className="font-secondary text-secondary mb-2">
+          Pitatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo
+          enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit,
+          sed quia
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default OurPerformance;
--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,12 +1,14 @@
 import AboutBanner from "../Components/AboutBanner";
 import AboutSubscribe from "../Components/AboutSubscribe";
 import Container from "../Components/Container";
+import OurPerformance from "../Components/OurPerformance";
 
 const About = () => {
   return (
     <>
       <Container>
         <AboutBanner />
+        <OurPerformance />
         <AboutSubscribe />
       </Container>
     </>
